ff8016a0feccdc0e90277b0d88492d6f19b4e354
chvvasss <yagizemirbaki61@gmail.com>
2025-12-19 23:15:47 +0300

    ilk halini yüklüyorız

---
+++ products/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import product_list_create, product_detail
+
+urlpatterns = [
+    path("products/", product_list_create, name="product-list-create"),
+    path("products/<int:pk>/", product_detail, name="product-detail"),
+]
